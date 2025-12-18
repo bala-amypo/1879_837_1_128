c4612884a299791434577080b40be9ff029bbd51
bala-amypo <bala@amypo.in>
2025-12-18 14:43:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/HoldingRecord.java
@@ -0,0 +1,66 @@
+package com.example.demo.entity;
+
+import com.example.demo.entity.enums.AssetClassType;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class HoldingRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long investorId;
+
+    @Enumerated(EnumType.STRING)
+    private AssetClassType assetClass;
+
+    private Double currentValue;
+
+    private LocalDateTime snapshotDate = LocalDateTime.now();
+
+
+    public HoldingRecord() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getInvestorId() {
+        return investorId;
+    }
+
+    public void setInvestorId(Long investorId) {
+        this.investorId = investorId;
+    }
+
+    public AssetClassType getAssetClass() {
+        return assetClass;
+    }
+
+    public void setAssetClass(AssetClassType assetClass) {
+        this.assetClass = assetClass;
+    }
+
+    public Double getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(Double currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public LocalDateTime getSnapshotDate() {
+        return snapshotDate;
+    }
+
+    public void setSnapshotDate(LocalDateTime snapshotDate) {
+        this.snapshotDate = snapshotDate;
+    }
+}
