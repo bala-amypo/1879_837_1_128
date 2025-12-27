f5ddfc0a0b1a5f4f12754bea2ef26fda7aa52ce6
bala-amypo <bala@amypo.in>
2025-12-27 07:41:54 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -11,15 +11,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                        "/v3/api-docs/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html"
-                ).permitAll()
                 .anyRequest().permitAll()
             )
             .httpBasic(Customizer.withDefaults())
